20347b2cfe070e5597f43cd00e3918340f2142ad
Mayan Dayani <mayandayani@protonmail.com>
2023-11-22 20:16:18 -0500

    created basic code for HTML, CSS, JS. TODO JS page

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+PREP
+
+Parameters
+16 input variables
+
+10 numbers => function(x) push x to display and keeps pop() to it until max?
+5 operators => arguement to display; if nothing in display null; 
+else if number operate and proceed
+
+1 dot . => push . even if no number
+
+Returns
+
+
+Example
+Pseudocode
+
+*/
+
